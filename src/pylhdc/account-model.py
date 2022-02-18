0e008abf422f6bb7eb1aad29a41b936f0021ad5a
Guillaume BITON <guillaume@gbweb.fr>
2022-02-18 23:02:49 +0100

    Filled account and transaction models

--- src/pylhdc/account-model.py
+++ src/pylhdc/account-model.py
@@ -1,15 +1,84 @@
-from datetime import date
-from locale import currency
-import numbers
-from tokenize import group
+class Account:
 
+    def __init__(self):
+        self._id = None
+        self._type = None
+        self._group = None
+        self._bank = None
+        self._number = None
+        self._initial_balance = None
+        self._initial_balance_date = None
+        self._currency = None
+        self._overdraft = None
 
-id
-type
-group
-bank
-number
-initial-balance
-initial-balance-date
-currency
-overdraft
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        self._type = type
+
+    @property
+    def group(self):
+        return self._group
+
+    @group.setter
+    def group(self, group):
+        self._group = group
+
+    @property
+    def bank(self):
+        return self._bank
+
+    @bank.setterNone
+    def bank(self, bank):
+        self._bank = bank
+
+    @property
+    def number(self):
+        return self._number
+
+    @number.setter
+    def number(self, number):
+        self._number = number
+
+    @property
+    def initial_balance(self):
+        return self._initial_balance
+
+    @initial_balance.setter
+    def initial_balance(self, initial_balance):
+        self._initial_balance = initial_balance
+
+    @property
+    def initial_balance_date(self):
+        return self._initial_balance_date
+
+    @initial_balance_date.setter
+    def initial_balance_date(self, initial_balance_date):
+        self._initial_balance_date = initial_balance_date
+
+    @property
+    def currency(self):
+        return self._currency
+
+    @currency.setter
+    def currency(self, currency):
+        self._currency = currency
+
+    @property
+    def overdraft(self):
+        return self._overdraft
+
+    @overdraft.setter
+    def overdraft(self, overdraft):
+        self._overdraft = overdraft
--- src/pylhdc/transaction-model.py
+++ src/pylhdc/transaction-model.py
@@ -1,16 +1,119 @@
-from xml.dom import ValidationErr
+class Transaction:
 
+    def __init__(self):
+        self._id = None
+        self._parent = None
+        self._date = None
+        self._source_account = None
+        self._destination_account = None
+        self._amount = None
+        self._title = None
+        self._mean_of_payment = None
+        self._third_party = None
+        self._info = None
+        self._category = None
+        self._validation = None
+        self._status = None
 
-id
-parent
-date
-source-account
-destination-account
-amount
-title
-mean-of-payment
-third-party
-info
-category
-validation  (check)
-status (warning)
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        self._date = date
+
+    @property
+    def source_account(self):
+        return self._source_account
+
+    @source_account.setter
+    def source_account(self, source_account):
+        self._source_account = source_account
+
+    @property
+    def destination_account(self):
+        return self._destination_account
+
+    @destination_account.setter
+    def destination_account(self, destination_account):
+        self._destination_account = destination_account
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+    @property
+    def mean_of_payment(self):
+        return self._mean_of_payment
+
+    @mean_of_payment.setter
+    def mean_of_payment(self, mean_of_payment):
+        self._mean_of_payment = mean_of_payment
+
+    @property
+    def third_party(self):
+        return self._third_party
+
+    @third_party.setter
+    def third_party(self, third_party):
+        self._third_party = third_party
+
+    @property
+    def info(self):
+        return self._info
+
+    @info.setter
+    def info(self, info):
+        self._info = info
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category):
+        self._category = category
+
+    @property
+    def validation(self):
+        return self._validation
+
+    @validation.setter
+    def validation(self, validation):
+        self._validation = validation
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
